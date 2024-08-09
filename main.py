6a5a84e25d312340433d5806af7b26f67ec1afdc
DevSmapy <lyk456@naver.com>
2024-08-09 22:56:54 +0900

    feat: Add `save_data` part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scripts.data_ingestion import generate_params, fetch_multiple_pages
-from scripts.data_processing import process_multiple_pages
+from scripts.data_processing import process_multiple_pages, save_data
 from pyspark.sql import SparkSession
 
 
@@ -9,14 +9,14 @@ def main():
 
     params = generate_params(
         key="",  # Add your key here
-        openStartDt="1919",  # Add your `open` start date here
-        openEndDt="1950",  # Add your `open` end date here
+        openStartDt="2000",  # Add your `open` start date here
+        openEndDt="2023",  # Add your `open` end date here
         itemPerPage="100",  # Add your item per page here
     )
 
     data = fetch_multiple_pages(api_url, params=params)
     df = process_multiple_pages(data, spark).drop(*["directors", "companys"])
-    df.show()
+    save_data(df, partition=["prdtYear", "repGenreNm"], path="data/movies")
     spark.stop()
 
 
